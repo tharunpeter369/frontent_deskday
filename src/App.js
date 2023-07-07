c9f017f0431b06a0c3329634effda023e49aedf7
tharun <tharunpeter369@gmail.com>
2023-07-08 00:56:46 +0530

    progressing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
+    // <div>
+    //   <h1>hello</h1>
+    // </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        {/* <Route path="/hooks" element={<Hooks />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
